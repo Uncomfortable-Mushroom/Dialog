c08db021437b71ab2c473a2710ed30b9040f141e
hym <1349456160@qq.com>
2017-09-07 08:12:09 +0800

    用户名，密码修改完成

--- app/src/main/java/com/example/lenovo/myapplication/hello.java
+++ app/src/main/java/com/example/lenovo/myapplication/hello.java
@@ -19,56 +19,59 @@ public class hello extends AppCompatActivity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
-        Button button1=(Button) findViewById(R.id.button1);
+        Button button1 = (Button) findViewById(R.id.button1);
         button1.setOnClickListener(this);
-        Button button2=(Button) findViewById(R.id.button2);
+        Button button2 = (Button) findViewById(R.id.button2);
         button2.setOnClickListener(this);
     }
+
     @Override
-    public void onClick(View v){
-        switch(v.getId()){
+    public void onClick(View v) {
+        switch (v.getId()) {
             case R.id.button1:
-              show1();
+                show1();
                 break;
             case R.id.button2:
-              show2();
+                show2();
                 break;
             default:
                 break;
         }
     }
-    public void show1(){
+
+    public void show1() {
         final AlertDialog.Builder dialog
-                =new AlertDialog.Builder(hello.this);
+                = new AlertDialog.Builder(hello.this);
         dialog.setTitle("提示信息");
         dialog.setMessage("请单击登录按钮");
         dialog.setCancelable(false);
-        dialog.setPositiveButton("确定",new DialogInterface.OnClickListener(){
+        dialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog,int which){
+            public void onClick(DialogInterface dialog, int which) {
             }
         });
         dialog.show();
     }
-    public void show2(){
-        LayoutInflater inflater=LayoutInflater.from(hello.this);
-        final View view=inflater.inflate(R.layout.h,null);
+
+    public void show2() {
+        LayoutInflater inflater = LayoutInflater.from(hello.this);
+        final View view = inflater.inflate(R.layout.h, null);
         final AlertDialog.Builder builder
-                =new AlertDialog.Builder(hello.this);
+                = new AlertDialog.Builder(hello.this);
         builder.setTitle("登录界面");
         builder.setView(view);
         builder.setPositiveButton("登陆"
-                ,new DialogInterface.OnClickListener(){
-            @Override
-            public void onClick(DialogInterface builder,int Whichbutton){
-                final EditText user=view.findViewById(R.id.user);
-                final EditText pass=view.findViewById(R.id.pass);
-                if(user.getText().toString().equals("abc")&&pass.getText().toString().equals("123"))
-                    Toast.makeText(hello.this, "登录成功", Toast.LENGTH_SHORT).show();
-                else
-                    Toast.makeText(hello.this, "登录失败", Toast.LENGTH_SHORT).show();
-            }
-        });
+                , new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface builder, int Whichbutton) {
+                        final EditText user = view.findViewById(R.id.user);
+                        final EditText pass = view.findViewById(R.id.pass);
+                        if (user.getText().toString().equals("abc") && pass.getText().toString().equals("123"))
+                            Toast.makeText(hello.this, "登录成功", Toast.LENGTH_SHORT).show();
+                        else
+                            Toast.makeText(hello.this, "登录失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
         builder.show();
 
 
